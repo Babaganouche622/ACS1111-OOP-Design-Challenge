a35c93ce2e54a37804350b2ebdffad07fee2f127
Brian <briangordon.cahill@students.dominican.edu>
2022-11-28 17:11:33 -0600

    Built starting files and some comments to begin.

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+Pygame, import all things pygame, build the screen
+
+class1 = GameObject => Character => Player
+                                 => Enemy
+                    => Items
+
+methods, just print statements
+
+-Spaceship
+    health, lives, deaths, score, powerups
+-enemies
+    health, lives, deaths,
+-powerups
+
+-score counter, death counter, lives counter, music, sound effects, bullet classes, shooting mechanics
+
+"""
